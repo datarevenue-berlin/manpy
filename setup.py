97474699c36e59f9ead60de23795b9f49276ac8d
Giulio Tirabassi <giulio@datarevenue.com>
2020-06-24 20:11:04 +0200

    Update setup requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['simpy', 'pandas', 'xlwt']
+requirements = ['simpy', 'pandas', 'xlwt', 'zope']
 
 setup_requirements = ['pytest-runner', ]
 
